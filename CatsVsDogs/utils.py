e0a5fa44d20965d4884973e78ba72d9fcf6ad219
jmsalazarb <144841696+jmsalazarb@users.noreply.github.com>
2025-05-08 22:23:14 -0500

    reformatted utils to run on mac/linux

--- CatsVsDogs/utils.py
+++ CatsVsDogs/utils.py
@@ -8,20 +8,20 @@ def train_test_split(src_folder):
 	bad_files_dog = ['Thumbs.db', '11702.jpg']
 
 	# **ADD YOUR CODE HERE*
-	train_cat_dir = os.path.join(src_folder, 'Train', 'Cat')
-	train_dog_dir = os.path.join(src_folder, 'Train', 'Dog')
-	test_cat_dir = os.path.join(src_folder, 'Test', 'Cat')
-	test_dog_dir = os.path.join(src_folder, 'Test', 'Dog')
+	train_cat = os.path.join(src_folder, 'Train', 'Cat')
+	train_dog = os.path.join(src_folder, 'Train', 'Dog')
+	test_cat = os.path.join(src_folder, 'Test', 'Cat')
+	test_dog = os.path.join(src_folder, 'Test', 'Dog')
 
-	for directory in [train_cat_dir, train_dog_dir, test_cat_dir, test_dog_dir]:
+	for directory in [train_cat, train_dog, test_cat, test_dog]:
 		if not os.path.exists(directory):
 			os.makedirs(directory)
 	
-	cat_dir = os.path.join(src_folder, 'Cat')
-	dog_dir = os.path.join(src_folder, 'Dog')
+	cat = os.path.join(src_folder, 'Cat')
+	dog = os.path.join(src_folder, 'Dog')
 
-	cat_images = [img for img in os.listdir(cat_dir) if img not in bad_files_cat]
-	dog_images = [img for img in os.listdir(dog_dir) if img not in bad_files_dog]
+	cat_images = [img for img in os.listdir(cat) if img not in bad_files_cat]
+	dog_images = [img for img in os.listdir(dog) if img not in bad_files_dog]
 	
 	split_cat = int(len(cat_images) * 0.8)
 	split_dog = int(len(dog_images) * 0.8)
@@ -33,24 +33,24 @@ def train_test_split(src_folder):
 	test_dogs = dog_images[split_dog:]
 	
 	for img in train_cats:
-		src = os.path.join(cat_dir, img)
-		dst = os.path.join(train_cat_dir, img)
-		os.system(f'copy "{src}" "{dst}"')
+		src = os.path.join(cat, img)
+		dst = os.path.join(train_cat, img)
+		os.system(f'cp "{src}" "{dst}"')
 
 	for img in test_cats:
-		src = os.path.join(cat_dir, img)
-		dst = os.path.join(test_cat_dir, img)
-		os.system(f'copy "{src}" "{dst}"')
+		src = os.path.join(cat, img)
+		dst = os.path.join(test_cat, img)
+		os.system(f'cp "{src}" "{dst}"')
 	
 	for img in train_dogs:
-		src = os.path.join(dog_dir, img)
-		dst = os.path.join(train_dog_dir, img)
-		os.system(f'copy "{src}" "{dst}"')
+		src = os.path.join(dog, img)
+		dst = os.path.join(train_dog, img)
+		os.system(f'cp "{src}" "{dst}"')
 
 	for img in test_dogs:
-		src = os.path.join(dog_dir, img)
-		dst = os.path.join(test_dog_dir, img)
-		os.system(f'copy "{src}" "{dst}"')
+		src = os.path.join(dog, img)
+		dst = os.path.join(test_dog, img)
+		os.system(f'cp "{src}" "{dst}"')
 
 	# remove corrupted exif data from the dataset
 	remove_exif_data(src_folder+'Train/')
